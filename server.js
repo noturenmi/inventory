2f6fdc28975d5c226f9478dc69bc85532db29fb1
noturenmi <msantos02503@gmail.com>
2025-11-30 23:33:51 +0800

    Initial Commit

--- server.js
+++ server.js
@@ -12,14 +12,9 @@ app.use(express.json());
 app.use(cors());
 
 // --- MongoDB Connection ---
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/inventory';
-
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('✔ Connected to MongoDB'))
-.catch(err => console.error('✖ MongoDB connection error:', err));
+mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/inventory')
+  .then(() => console.log('✔ Connected to MongoDB'))
+  .catch(err => console.error('✖ MongoDB connection error:', err));
 
 // --- Mongoose Schema ---
 const productSchema = new mongoose.Schema({
