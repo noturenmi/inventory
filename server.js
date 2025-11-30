7bc6686f176f0d4437c08f3b249876f525b38365
noturenmi <msantos02503@gmail.com>
2025-12-01 02:29:08 +0800

    Initial Commit

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection (Atlas)
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
