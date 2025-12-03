bc9457dac1f2b2287a1831c8d68ce9bd55f55acb
noturenmi <msantos02503@gmail.com>
2025-12-04 03:11:50 +0800

    Initial Comitt

--- api/swagger.js
+++ api/swagger.js
@@ -1,11 +1,23 @@
-import path from "path";
-import { fileURLToPath } from "url";
+const express = require('express');
+const path = require('path');
+const swaggerUi = require('swagger-ui-express');
+const fs = require('fs');
 
-export default function handler(req, res) {
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = path.dirname(__filename);
+const app = express();
 
-  const filePath = path.join(__dirname, "..", "public", "swagger", "swagger.json");
-  res.setHeader("Content-Type", "application/json");
-  res.sendFile(filePath);
+// Serve the swagger.json
+const swaggerFilePath = path.join(__dirname, '..', 'public', 'swagger', 'swagger.json');
+let swaggerDocument = {};
+try {
+  swaggerDocument = JSON.parse(fs.readFileSync(swaggerFilePath, 'utf8'));
+} catch (err) {
+  console.error('Failed to read swagger.json:', err);
 }
+
+// Serve Swagger UI at /api/swagger
+app.use('/api/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Optional: redirect root of this function to Swagger UI
+app.get('/api/swagger', (req, res) => res.redirect('/api/swagger/'));
+
+module.exports = app;
