8c528c957dcd9c11a584aae6f1fcf7588aadaac2
noturenmi <msantos02503@gmail.com>
2025-12-04 03:18:59 +0800

    Initial Commit

--- api/swagger.js
+++ api/swagger.js
@@ -1,23 +1,8 @@
-const express = require('express');
-const path = require('path');
 const swaggerUi = require('swagger-ui-express');
-const fs = require('fs');
+const express = require('express');
+const swaggerDocument = require('../public/swagger/swagger.json');
 
-const app = express();
+const router = express();
+router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-// Serve the swagger.json
-const swaggerFilePath = path.join(__dirname, '..', 'public', 'swagger', 'swagger.json');
-let swaggerDocument = {};
-try {
-  swaggerDocument = JSON.parse(fs.readFileSync(swaggerFilePath, 'utf8'));
-} catch (err) {
-  console.error('Failed to read swagger.json:', err);
-}
-
-// Serve Swagger UI at /api/swagger
-app.use('/api/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-// Optional: redirect root of this function to Swagger UI
-app.get('/api/swagger', (req, res) => res.redirect('/api/swagger/'));
-
-module.exports = app;
+module.exports = router;
