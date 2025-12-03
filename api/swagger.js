835658662a3c6ed71929fa23bdf3f1ec5fa49a01
noturenmi <msantos02503@gmail.com>
2025-12-04 02:18:46 +0800

    Initial Commit

--- api/swagger.js
+++ api/swagger.js
@@ -1,8 +1,5 @@
-import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from '../../public/swagger/swagger.json';
-import express from 'express';
 
-const app = express();
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-export default app;
+export default function handler(req, res) {
+  res.status(200).json(swaggerDocument);
+}
