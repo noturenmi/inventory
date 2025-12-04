792f3da9c69f7ecd0f8b33c090579d6f90c1b543
noturenmi <msantos02503@gmail.com>
2025-12-04 20:25:32 +0800

    Initial Commit

--- api/swagger.js
+++ api/swagger.js
@@ -1,5 +1,27 @@
-import swaggerDocument from '../../public/swagger/swagger.json';
+const handler = async (req, res) => {
+  // CORS headers
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  
+  if (req.method === 'GET') {
+    return res.status(200).json({
+      message: "Swagger documentation available at:",
+      endpoints: {
+        ui: "/swagger",
+        json: "/swagger.json",
+        api_docs: "/api-docs"
+      },
+      note: "Visit /swagger for interactive API documentation"
+    });
+  }
+  
+  return res.status(405).json({ error: 'Method not allowed' });
+};
 
-export default function handler(req, res) {
-  res.status(200).json(swaggerDocument);
-}
+module.exports = handler;
