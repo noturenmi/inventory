494a62dd2e5a9071ce804e9c177e0b3cdb094405
noturenmi <msantos02503@gmail.com>
2025-12-04 21:55:23 +0800

    Initial Commit

--- api/index.js
+++ api/index.js
@@ -5,11 +5,33 @@ const handler = async (req, res) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
+  if (req.method === 'OPTIONS') return res.status(200).end();
+  
+  // Check if requesting Swagger
+  if (req.url === '/swagger' || req.url === '/swagger/') {
+    res.setHeader('Content-Type', 'text/html');
+    return res.send(`
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title>Inventory API Docs</title>
+          <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5.9.0/swagger-ui.css">
+        </head>
+        <body>
+          <div id="swagger-ui"></div>
+          <script src="https://unpkg.com/swagger-ui-dist@5.9.0/swagger-ui-bundle.js"></script>
+          <script>
+            SwaggerUIBundle({
+              url: '/swagger.json',
+              dom_id: '#swagger-ui'
+            });
+          </script>
+        </body>
+      </html>
+    `);
   }
   
-  // Return JSON for API clients
+  // Regular API response
   res.status(200).json({ 
     message: "📦 Inventory API running!",
     endpoints: {
