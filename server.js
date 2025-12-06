b1c2cbe98f72d19d094fa639f7cc75b28bde6d65
noturenmi <msantos02503@gmail.com>
2025-12-06 21:35:27 +0800

    Initial Commit

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ const app = express();
 // Middlewares
 app.use(cors());
 app.use(express.json());
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI)
