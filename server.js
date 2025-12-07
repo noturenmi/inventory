77c039015787a56f4fca8902639cd53adc11c176
noturenmi <msantos02503@gmail.com>
2025-12-07 16:42:16 +0800

    Initial Commit

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
 const app = express();
 app.use(cors());
